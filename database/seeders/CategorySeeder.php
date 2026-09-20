2d0c041cd1cdaf6e622b42c60cda71abf439acdc
Delano240404 <2410631170015@student.unsika.ac.id>
2026-09-20 23:23:35 +0700

    mengikuti praktikum pertemuan 3: database migration dan seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    public function run(): void
+    {
+        $categories = ['Sembako', 'Minuman', 'Makanan Ringan', 'Kebutuhan Rumah Tangga'];
+
+        foreach ($categories as $name) {
+            DB::table('categories')->insert([
+                'name' => $name,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
